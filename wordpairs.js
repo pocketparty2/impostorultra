d8a8a7196e06563aafb62efdd13a6e133804b257
pocketparty2 <ilikefindtheshampoos@gmail.com>
2026-04-19 23:54:36 +0100

    Add WORD_PAIRS array with real and impostor items

---
+++ wordpairs.js
@@ -0,0 +1,7 @@
+const WORD_PAIRS = [
+  { real: "Cat", impostor: "Lion" },
+  { real: "Beach", impostor: "Desert" },
+  { real: "Pizza", impostor: "Lasagna" },
+  { real: "Car", impostor: "Motorbike" },
+  { real: "Moon", impostor: "Planet" }
+];
